1986ef6f033c7a73aea710c8ccd2a9ed8997db76
Pauline <lynnsulit@gmail.com>
2021-12-15 20:37:02 -0500

    first js commit

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@
 
 // 1. Create a variable for the hamburger menu
 // 2. Add an event listener, specifically listening for the hamburger menu to be clicked
-// 3. Show the drop down menu (add HTML) when clicked
 
 const hamburgerMenu = document.querySelector(".fa-bars");
 
@@ -10,12 +9,6 @@ const leftNavBar = document.querySelector('.leftNav');
 const rightNavBar = document.querySelector('.rightNav');
 
 hamburgerMenu.addEventListener('click', function(event) {
-    // const dropDownMenu = document.querySelector(".dropDown");
-    // navBar.classList.toggle('dropDown');
-    // console.log(navBar);
-
-    //   leftNavBar.style.display = "block";
-    //   rightNavBar.style.display = "block";
 
     if (leftNavBar.style.display === "block" && rightNavBar.style.display === "block") {
         leftNavBar.style.display = "none";
@@ -25,13 +18,4 @@ hamburgerMenu.addEventListener('click', function(event) {
         leftNavBar.style.display = "block";
         rightNavBar.style.display = "block";
     };
-
-    // if (leftNavBar.style.display === "none" && rightNavBar.style.display === "none") {
-    //     leftNavBar.style.display = "block";
-    //     rightNavBar.style.display = "block";
-
-    // } else {
-    //     leftNavBar.style.display = "none";
-    //     rightNavBar.style.display = "none";
-    // };
 });
